2707ce122f9a05031a7f77de97cfc652fe5f94a5
Adam Wester <awwester@gmail.com>
2016-10-11 18:57:13 -0500

    message sent after instance saved

--- processes/consumers.py
+++ processes/consumers.py
@@ -2,23 +2,25 @@ from channels import Channel, Group
 from channels.sessions import channel_session
 from channels.auth import http_session_user, channel_session_user, channel_session_user_from_http
 
-# Connected to websocket.connect
+from .models import Process
+
+
 @channel_session_user_from_http
 def ws_add(message):
     # Accept connection
     message.reply_channel.send({"accept": True})
 
     # Add them to the right group
-    Group("chat-%s" % message.user.id).add(message.reply_channel)
+    Group("chat").add(message.reply_channel)
 
 # Connected to websocket.receive
 @channel_session_user
 def ws_message(message):
-    Group("chat-%s" % message.user.id).send({
+    Group("chat").send({
         "text": message['text'],
     })
 
 # Connected to websocket.disconnect
 @channel_session_user
 def ws_disconnect(message):
-    Group("chat-%s" % message.user.id).discard(message.reply_channel)
+    Group("chat").discard(message.reply_channel)
--- processes/models.py
+++ processes/models.py
@@ -1,8 +1,26 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.conf import settings
+
+from channels import Group
 
 
 class Process(models.Model):
     name = models.CharField(max_length=20)
     complete = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+@receiver(post_save, sender=Process, dispatch_uid="process_saved")
+def send_message(sender, instance, **kwargs):
+    """
+    after the process instance is saved, send a message to its user
+    """
+    print('sending message... ')
+    Group("chat").send({
+        "text": "process was saved!",
+    })
