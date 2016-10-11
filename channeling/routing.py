59756aad49c3179230b5fc9d611aa82a6ce937d5
Adam Wester <awwester@gmail.com>
2016-10-11 18:00:55 -0500

    channel groups

--- channeling/routing.py
+++ channeling/routing.py
@@ -1,6 +1,9 @@
 from channels.routing import route
+from processes.consumers import ws_add, ws_message, ws_disconnect
 
 
 channel_routing = [
-    route("http.request", "processes.consumers.http_consumer"),
+    route("websocket.connect", ws_add),
+    route("websocket.receive", ws_message),
+    route("websocket.disconnect", ws_disconnect),
 ]
